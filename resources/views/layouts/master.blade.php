64a74882f9b0b4d0a4ff8228f790ade4eadcc561
Adriana Rossetti <adriana.rossetti@icloud.com>
2019-04-18 15:15:01 +0900

    master file, fixed meta

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,8 +3,9 @@
 <html lang='en'>
 
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-    <meta charset='UTF-8'>
+    {{--<meta charset='UTF-8'>--}}
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <meta name='csrf-token' content='{{ csrf_token() }}'>
 
