28635572eec18e3ccb91288f2494d34e7b6cee94
Adriana Rossetti <adriana.rossetti@icloud.com>
2019-04-30 04:46:49 +0100

    Fixed broken link image

--- resources/views/reviews/search.blade.php
+++ resources/views/reviews/search.blade.php
@@ -108,7 +108,7 @@
                                         <p class='d-inline'>Be the first to rate this course</p>
                                         <img id='hand-rating'
                                              class='d-inline'
-                                             src='/images/icons/rating.png'
+                                             src='/images/show/rating.png'
                                              alt='Hand clicking on stars'>
 
                                     </div>
