18ce62f2095116b033cc8616ecf1b9df30deae68
Adriana Rossetti <adriana.rossetti@icloud.com>
2019-04-30 04:29:41 +0100

    Udated composer.lock only

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-04-24 17:29:03.
+    * Generated on 2019-04-30 03:26:54.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.28 on 2019-04-24 17:29:03.
+ * Generated for Laravel 5.7.28 on 2019-04-30 03:26:54.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
