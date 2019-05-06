fe9314bab03b68aa3f52edca428a7e2e101d8f36
Adriana Rossetti <adriana.rossetti@icloud.com>
2019-05-06 03:46:26 +0100

    Composer update

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-04-30 03:26:54.
+    * Generated on 2019-05-06 02:45:55.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
